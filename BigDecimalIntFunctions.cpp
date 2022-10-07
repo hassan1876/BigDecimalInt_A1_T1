9c5588b9bc664980fbe840adb8b1b072f1380f38
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-10-07 18:34:11 +0200

    Uploaded empty project.

--- BigDecimalIntFunctions.cpp
+++ BigDecimalIntFunctions.cpp
@@ -2,3 +2,47 @@
 
 // Defining functions, class methods
 
+
+BigDecimalInt::BigDecimalInt(string decStr) {
+
+}
+
+BigDecimalInt::BigDecimalInt(int decInt) {
+
+}
+
+BigDecimalInt BigDecimalInt::operator+(BigDecimalInt anotherDec) {
+//    return ;
+}
+
+BigDecimalInt BigDecimalInt::operator-(BigDecimalInt anotherDec) {
+//    return ;
+}
+
+bool BigDecimalInt::operator<(BigDecimalInt anotherDec) {
+    return false;
+}
+
+bool BigDecimalInt::operator>(BigDecimalInt anotherDec) {
+    return false;
+}
+
+bool BigDecimalInt::operator==(BigDecimalInt anotherDec) {
+    return false;
+}
+
+BigDecimalInt BigDecimalInt::operator=(BigDecimalInt anotherDec) {
+//    return ;
+}
+
+int BigDecimalInt::size() {
+    return 0;
+}
+
+int BigDecimalInt::sign() {
+    return 0;
+}
+
+ostream &operator<<(ostream &out, BigDecimalInt b) {
+//    return ;
+}
--- BigDecimalIntHeader.h
+++ BigDecimalIntHeader.h
@@ -8,3 +8,25 @@
 using namespace std;
 
 // Prototypes, Constants and Classes
+
+class BigDecimalInt {
+private:
+    string decStr;
+    int decInt;
+
+public:
+    inline BigDecimalInt(string decStr); // Initializes from string & rejects bad input
+    inline BigDecimalInt(int decInt); // Initialize from integer
+
+    inline BigDecimalInt operator+(BigDecimalInt anotherDec); // member fn
+    inline BigDecimalInt operator-(BigDecimalInt anotherDec); // member fn
+    inline bool operator<(BigDecimalInt anotherDec); // member fn
+    inline bool operator>(BigDecimalInt anotherDec); // member fn
+    inline bool operator==(BigDecimalInt anotherDec); // member fn
+    inline BigDecimalInt operator=(BigDecimalInt anotherDec); // member fn
+
+    inline int size(); // member fn
+    inline int sign(); // member fn
+
+    inline friend ostream &operator<<(ostream &out, BigDecimalInt b);
+};
